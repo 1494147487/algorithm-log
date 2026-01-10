03a164fe1773ffd342addeaebe01efb2ffa2f959
黄宇杰 <1494147487@qq.com>
2026-01-10 17:50:24 +0800

    -m bfs

--- 图/bfs797.cpp
+++ 图/bfs797.cpp
@@ -1,29 +1,30 @@
-//bool Solution::visit[100][100];
 class Solution {
 public:
-    bool visit[100]; 
-    void dfs(int u,int sz,vector<vector<int>>& result,vector<int>& res,vector<vector<int>>& graph){
-        if(u > sz)return;
-        if(visit[u])return;
-        res.push_back(u);
-        visit[u] = true;
-        if(u == sz){
-            result.push_back(res);
-        }
-        for(int i :graph[u]){
-            dfs(i,sz,result,res,graph);
-        }
-        res.pop_back();
-        visit[u] = false;
-
-   }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>>result;
-        vector<int>res;
-        int sz = graph.size() - 1;
-       // for(int i :graph[0]){
-            dfs(0,sz,result,res,graph);
-      // }
+        vector<vector<int>>result;//创建接收路径的二维数组
+        queue<vector<int>>pathqueue;//创建一个装路径的队列
+        queue<int>nodequeue;//创建一个装节点的队列
+        pathqueue.push({0});//初始化
+        nodequeue.push(0);//初始化
+        int target = graph.size() - 1;//拿出终止节点
+
+        while(!nodequeue.empty()){
+            vector<int> currentpath = pathqueue.front();
+            int cur = nodequeue.front();
+            pathqueue.pop();
+            nodequeue.pop();
+            if(cur == target){//如果到达终点
+                result.push_back(currentpath);//加入完整的一条路径
+                continue;//直接下一轮，再往下没意义
+            }
+            for(int i:graph[cur]){//访问该节点的邻居
+                vector<int>newpath = currentpath;//创建新路径，currentpath要留着下轮循环用，不能改
+                nodequeue.push(i);//推入节点
+                newpath.push_back(i);//拓展路径
+                pathqueue.push(newpath);//把路径专门推入装路径的队列
+            }
+
+        }
         return result;
     }
 };
