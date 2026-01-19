b4ced7d4903b2c84d5bac9a93cc08fad8c4d7a49
黄宇杰 <1494147487@qq.com>
2026-01-19 14:30:52 +0800

    1.19

--- 排序/堆排序912.cpp
+++ 排序/堆排序912.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
 class Teap {//根节点对应数组下标1，最新加的叶节点对应数组数量值的下标
 	private:
 		vector<int> arr;
@@ -6,6 +10,21 @@ class Teap {//根节点对应数组下标1，最新加的叶节点对应数组�
 		Teap():arr(111111) {  //构造函数
 			size = 0;
 		}
+
+		// 新增：原地建堆构造函数
+		//Teap(vector<int>& nums) : arr(111111) {
+		//	// 将数据复制到arr中（从下标1开始）
+		//	size = nums.size();
+		//	for (int i = 0; i < size; i++) {
+		//		arr[i + 1] = nums[i];
+		//	}
+
+		//	// Floyd建堆法：O(n)时间建堆，直接调用你的sink函数
+		//	for (int i = size / 2; i >= 1; i--) {
+		//		sink(i);  // 直接使用你的sink函数
+		//	}
+		//}
+
 		int right(int i) {
 			return i * 2 + 1;
 		}
@@ -54,17 +73,16 @@ class Teap {//根节点对应数组下标1，最新加的叶节点对应数组�
 		}
 };
 
-class Solution {
-public:
-    vector<int> sortArray(vector<int>& nums) {
-        Teap teap;
-        int n = nums.size();
-        for(int i = 0;i<n;i++){
-            teap.add(nums[i]);
-        }
-        for(int i = 0;i<n;i++){
-           nums[i] = teap.pop();
-        }
-        return nums;
-    }
-};
+int main() {
+
+	Teap teap;
+	teap.add(3);
+	teap.add(4);
+	teap.add(1);
+	teap.add(2);
+	cout << teap.pop() << endl;
+	cout << teap.pop() << endl;
+	cout << teap.pop() << endl;
+	cout << teap.pop() << endl;
+
+}
